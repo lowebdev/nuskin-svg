8ee7394582363c66091abba09b69abcd2a418091
Olivier Lepage <hello@olivierlepage.dev>
2020-06-09 15:53:53 -0400

    + Add reset color command
    - Support single attribute reset or all attributes reset (for single file or entire directory)

--- bin/index.js
+++ bin/index.js
@@ -5,9 +5,8 @@ Array.prototype.indexOrUndefined = function(index) {
 
 const fs = require('fs')
 const path = require('path')
-// const fillRegex = /fill="([#\(,\)]|[A-z]|[0-9])+"/g
-const fillStyleRegex = /fill:([#\(,\) ]|[A-z]|[0-9])+;/g
-const styleTagRegex = /style="([#\(,:;\) ]|[A-z]|[0-9])+"/g
+const attributes = ['fill', 'stroke', 'background']
+const styleTagRegex = /\s*style="([#\(,:;\) ]|[A-z]|[0-9])+"/g
 const svgStartingTagRegex = /<svg(\s|[a-z]|[A-Z]|["=:;,.]|[^<])+/
 const yargs = require('yargs')
 
@@ -18,24 +17,27 @@ yargs
     console.log(`use 'nuskin --help' for a list of commands`)
   })
   // Recolor command
-  .command('recolor', 'Recolors .svgs\' fill color', (yargs) => {
-    return yargs.usage('Usage: nuskin recolor --path <path> --fill [fill color]')
-                .option('path', { alias: 'p', describe: 'Path to directory containing SVG files to be recolored', type: 'string', demandOption: true })
+  .command('recolor', 'Recolors a single (or all .svg in containing folder if path arg is a dir) .svg file\'s fill, background or stroke attribute', (yargs) => {
+    return yargs.usage('Usage: nuskin recolor --path <path> --attr [attribute to recolor] --color [color to apply to attribute]')
+                .option('path', { alias: 'p', describe: 'Path to file or directory containing SVG files to be recolored', type: 'string', demandOption: true })
                 .option('attr', { alias: 'a', describe: 'The attribute to apply the new color to. Supports "fill", "stroke" & "background". Defaults to "fill" ', type: 'string' })
                 .option('color', { alias: 'c', describe: 'The color you want to apply to svgs\' attribute (only supports CSS color values e.g.: blue, rgb(0,0,0), #beeeef)', type: 'string' })
   }, recolor)
+  .command('reset', 'Removes colors of a single (or all .svg in containing folder if path arg is a dir) .svg file\'s fill, background, stroke or all attribute(s)', (yargs) => {
+    return yargs.usage('Usage: nuskin reset --path <path> --attr [attribute to recolor] (--all resets all attributes to specified path)')
+                .option('path', { alias: 'p', describe: 'Path to file or directory containing SVG files to reset', type: 'string', demandOption: true })
+                .option('attr', { alias: 'a', describe: 'The attribute to reset. Supports "fill", "stroke", "background". If none is specified, all attributes will be reset', type: 'string' })
+  }, reset)
   .help()
   .argv
 
-// Arguments
-// 0: <path> Folder path containing SVG files
-// 1: [fill] Color replacement
 function recolor(argv) {
+
   const pathArg = argv.path
   const color = argv.color
   const attr = argv.attr || 'fill'
 
-  if (!['fill', 'stroke', 'background'].includes(attr))
+  if (!attributes.includes(attr))
     throw new Error('"attr" argument must have a value of "fill", "stroke" or "background"')
 
   if (!color)
@@ -44,7 +46,7 @@ function recolor(argv) {
   try {
     const filenames = getAbsoluteFilePaths(pathArg)
     const isSolo = filenames.length === 1
-    console.log(`Found ${filenames.length} SVG file${isSolo ? '' : 's'} at path.\r\n`)
+    console.log(`Found ${filenames.length} SVG file${isSolo ? '' : 's'} at provided path`)
 
     for (let i = 0; i < filenames.length; i++)
       recolorSvgAtAbsolutePath(filenames[i], color, attr)
@@ -55,6 +57,7 @@ function recolor(argv) {
 }
 
 function recolorSvgAtAbsolutePath(pathArg, color, attr) {
+
   const attrRegex = new RegExp(`${attr}:([#\(,\) ]|[A-z]|[0-9])+;`)
   let svgStrData = fs.readFileSync(pathArg).toString('utf8')
   let startSvgTagData = (svgStrData.match(svgStartingTagRegex) || []).indexOrUndefined(0)
@@ -78,11 +81,60 @@ function recolorSvgAtAbsolutePath(pathArg, color, attr) {
 
   // Overwrite file content with new data
   fs.writeFileSync(pathArg, svgStrData)
-  console.log('Successfully changed color of ' + path.basename(pathArg) + '!\r\n')
+  console.log('Successfully changed color of ' + path.basename(pathArg))
 }
 
+function reset(argv) {
+
+  const pathArg = argv.path
+  const attr = argv.attr
+
+  try {
+    const filenames = getAbsoluteFilePaths(pathArg)
+    const isSolo = filenames.length === 1
+    console.log(`Found ${filenames.length} SVG file${isSolo ? '' : 's'} at provided path`)
+
+    for (let i = 0; i < filenames.length; i++)
+      resetColorsAtPath(filenames[i], attr)
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+function resetColorsAtPath(pathArg, attribute) {
+
+  const attrRegex = new RegExp(`${attribute}:([#\(,\) ]|[A-z]|[0-9])+;`)
+  let svgStrData = fs.readFileSync(pathArg).toString('utf8')
+  let startSvgTagData = (svgStrData.match(svgStartingTagRegex) || []).indexOrUndefined(0)
+  let svgStyleTagData = ((startSvgTagData || '').match(styleTagRegex) || []).indexOrUndefined(0)
+  let targetStyle = ((svgStyleTagData || '').match(attrRegex) || []).indexOrUndefined(0)
+
+  if (!attribute && svgStyleTagData) {
+    // Did not specify attribute, reset all attributes
+    let newStartSvgTagData = startSvgTagData.replace(styleTagRegex, '')
+    svgStrData = svgStrData.replace(startSvgTagData, newStartSvgTagData)
+
+  } else if (targetStyle) {
+    // Has style tag & target attribute
+    let newSvgStyleTagData = svgStyleTagData.replace(targetStyle, '')
+    svgStrData = svgStrData.replace(svgStyleTagData, newSvgStyleTagData)
+
+  } else {
+    // 1- Has style tag and does not have target style attribute
+    // 2- Does not have style tag yet
+    console.log(`Cannot reset color because ${path.basename(pathArg)}'s target attribute does not exist`)
+    return
+  }
+
+  // Overwrite file content with new data
+  fs.writeFileSync(pathArg, svgStrData)
+  console.log('Successfully reset colors of ' + path.basename(pathArg))
+}
+
+// UTILS
 function getAbsoluteFilePaths(filepath) {
-  
+
   if (!path.isAbsolute(filepath)) {
     filepath = path.resolve(filepath)
   }
@@ -95,7 +147,7 @@ function getAbsoluteFilePaths(filepath) {
 }
 
 function getFolderContentInfo(folderName) {
-  
+
   const files = fs.readdirSync(folderName, { withFileTypes: true })
     .filter(dirent => {
       return dirent.isFile() && dirent.name.includes('.svg')
